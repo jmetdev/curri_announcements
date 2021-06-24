573ab62514c8a680b564755ae30eb7e3143c1641
jemetc1 <jmetcalf@hyetechnetworks.com>
2021-06-24 14:22:48 -0700

    Jinja Templating

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/<greetingName>', methods=['HEAD', 'POST', 'GET'])
+def hello_world(greetingName):
+    #return f'<p>Hello, {greetingName}! </p>'
+    return render_template('announcement.xml', name=greetingName)
+
+
+app.run()
