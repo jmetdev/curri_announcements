ef206dba827afe5676f7e55f0ea57081a9aac8ed
jemetc1 <jmetcalf@hyetechnetworks.com>
2021-06-24 15:26:38 -0700

    Linted Cleaned Up Coding

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route('/', methods=['HEAD'])
+@app.route("/", methods=["HEAD"])
 def head():
-    return ('', 204)
+    return ("", 204)
 
-@app.route('/<greetingName>', methods=['HEAD', 'POST', 'GET'])
+
+@app.route("/<greetingName>", methods=["HEAD", "POST", "GET"])
 def renderCURRI(greetingName):
-    return render_template('announcement.xml', name=greetingName)
+    return render_template("announcement.xml", name=greetingName)
 
 
 app.run()
